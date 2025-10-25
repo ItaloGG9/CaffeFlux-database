ec9f0ffb63785512c907f1a4cc81a458a4ee6cb1
ItaloGG9 <104172674+ItaloGG9@users.noreply.github.com>
2025-10-25 00:43:07 -0300

    Improve database cursor handling in ver_productos
    Refactor ver_productos function to use try-finally for cursor management.

--- manage.py
+++ manage.py
@@ -3,15 +3,22 @@ from database import get_db_connection
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "API de CaffeFlux conectada a PostgreSQL ✅"}
+
 @app.get("/productos")
 def ver_productos():
     conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM productos")
-    columnas = [desc[0] for desc in cursor.description]
-    productos = [dict(zip(columnas, fila)) for fila in cursor.fetchall()]
-    conn.close()
-    return productos
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM productos;")
+        columnas = [desc[0] for desc in cur.description]
+        productos = [dict(zip(columnas, fila)) for fila in cur.fetchall()]
+        cur.close()
+        return productos
+    finally:
+        conn.close()
 
 @app.get("/debug-db")
 def debug_db():
